e2b64856ab48ef8537ded410eb1ec53388934b62
Skylad7 <dylan.fowler7@yahoo.com>
2025-02-24 11:08:07 -0500

    Added new tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name    string
+		headers http.Header
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "no auth header",
+			headers: http.Header{},
+			want:    "",
+			wantErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:    "malformed header",
+			headers: http.Header{"Authorization": []string{"ApiKey"}},
+			want:    "",
+			wantErr: errors.New("malformed authorization header"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPIKey(tt.headers)
+			if (err != nil) != (tt.wantErr != nil) {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("GetAPIKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
