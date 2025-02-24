1b558c781d9d0bb94de981c309e6f5a4ae3958ab
Skylad7 <dylan.fowler7@yahoo.com>
2025-02-24 11:24:11 -0500

    Reverted tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -47,7 +47,7 @@ func TestGetAPIKey(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetAPIKey(tt.headers)
-			if tt.wantErr != nil && !errors.Is(err, tt.wantErr) {
+			if (err != nil) != (tt.wantErr != nil) {
 				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
