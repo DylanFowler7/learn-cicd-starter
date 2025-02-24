347cfc3c6e5e6d0dfb7d9619987e456536e2ac34
Skylad7 <dylan.fowler7@yahoo.com>
2025-02-24 11:10:14 -0500

    Broke Test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -29,6 +29,7 @@ func TestGetAPIKey(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetAPIKey(tt.headers)
+			got = ""
 			if (err != nil) != (tt.wantErr != nil) {
 				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
 				return
